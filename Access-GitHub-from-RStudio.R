8767387924ca832dd3e05be72433d5d506b9526a
GS45191 <desa.b@live.de>
2022-10-08 21:36:36 +0200

    Various changes

--- Access-GitHub-from-RStudio.R
+++ Access-GitHub-from-RStudio.R
@@ -3,6 +3,8 @@ library(usethis)
 use_github()
 usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
 
+create_from_github() 
+
 options(
   usethis.full_name = "Desa Berisha",
   usethis.protocol  = "ssh",
@@ -19,6 +21,24 @@ options(
   usethis.overwrite = TRUE
 )
 
+usethis::gh_token_help()
 usethis::git_sitrep()
 
+gitcreds::gitcreds_set()
+
 edit_r_environ()
+
+# Get a personal access token (PAT)
+usethis::create_github_token()
+
+
+usethis::edit_r_environ()
+
+gitcreds::gitcreds_set("https://github.acme.com")
+usethis::edit_r_environ()
+
+remotes::install_github("r-lib/usethis")
+
+pak::pkg_install("r-lib/usethis")
+
+Sys.setenv(GITHUB_PAT = gitcreds::gitcreds_get(use_cache = FALSE)$password)
